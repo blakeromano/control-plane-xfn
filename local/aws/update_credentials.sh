d36aa94eb8451cbff7a8b134d8cb3a8c9febeaac
Ken Ellinwood <ken.ellinwood@imaginelearning.com>
2024-11-12 08:34:01 -1000

    Fix sed separator for access_key_id and secret_access_key

--- local/aws/update_credentials.sh
+++ local/aws/update_credentials.sh
@@ -15,8 +15,8 @@ done
 git restore manifests/credentials.yaml
 
 cat manifests/credentials.yaml | \
-    sed "s/aws_access_key_id=REPLACE/aws_access_key_id=$AWS_ACCESS_KEY_ID/" | \
-    sed "s/aws_secret_access_key=REPLACE/aws_secret_access_key=$AWS_SECRET_ACCESS_KEY/" | \
+    sed "s!aws_access_key_id=REPLACE!aws_access_key_id=$AWS_ACCESS_KEY_ID!" | \
+    sed "s!aws_secret_access_key=REPLACE!aws_secret_access_key=$AWS_SECRET_ACCESS_KEY!" | \
     sed "s!aws_session_token=REPLACE!aws_session_token=$AWS_SESSION_TOKEN!" >manifests/credentials.yaml.tmp
 mv manifests/credentials.yaml.tmp manifests/credentials.yaml
 
